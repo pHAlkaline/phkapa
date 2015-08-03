db6fcc029da048fbe4d30a0fe88180e9160f3f77
jefflv <jefflv@gmail.com>
2015-08-03 14:00:50 -0400

    40-character username and password

--- app/Config/Schema/phkapa_structure_spa.sql
+++ app/Config/Schema/phkapa_structure_spa.sql
@@ -488,7 +488,7 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL DEFAULT '',
-  `username` varchar(24) DEFAULT NULL,
+  `username` varchar(40) DEFAULT NULL,
   `password` varchar(40) DEFAULT NULL,
   `email` varchar(256) DEFAULT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '0',
