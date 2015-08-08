ba2bbd4ae7a911a0c75cfc353021505e2b29f0f2
pHAlkaline <contact@phalkaline.net>
2015-08-08 01:04:43 +0100

    secure install controller

--- app/Config/core.php
+++ app/Config/core.php
@@ -192,12 +192,12 @@ Configure::write('Session', array(
 /**
  * A random string used in security hashing methods.
  */
-Configure::write('Security.salt', '6e85850b7f0f651fe6486ededf073a80ea1f78eb');
+Configure::write('Security.salt', 'zshlC2wMeCWMnRH8BmqmLQUFeBIT4uwBGSMS4k1w');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-Configure::write('Security.cipherSeed', '14213518131453323006');
+Configure::write('Security.cipherSeed', '2973937642728344649949541921993430529846');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
