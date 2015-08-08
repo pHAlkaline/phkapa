251f88f1f7c43428f375a8e671b87ac89a4fe460
pHAlkaline <contact@phalkaline.net>
2015-08-08 12:27:57 +0100

    Plugin FULL PACK info

--- app/Config/core.php
+++ app/Config/core.php
@@ -192,12 +192,12 @@ Configure::write('Session', array(
 /**
  * A random string used in security hashing methods.
  */
-Configure::write('Security.salt', 'zshlC2wMeCWMnRH8BmqmLQUFeBIT4uwBGSMS4k1w');
+Configure::write('Security.salt', 'zshlC2wMeCWMnRH8BmqmLQUFeBIT4uwBGSMS4k1w'); // default value: zshlC2wMeCWMnRH8BmqmLQUFeBIT4uwBGSMS4k1w
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-Configure::write('Security.cipherSeed', '2973937642728344649949541921993430529846');
+Configure::write('Security.cipherSeed', '2973937642728344649949541921993430529846'); // default value: 2973937642728344649949541921993430529846
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
