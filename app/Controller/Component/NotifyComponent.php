ae019840d436b2f8c36665e5faa27f58093ab05c
paulo <paulo.homem@gmail.com>
2015-08-21 12:17:56 +0100

    PHKAPA is now pHKapa on all "text" found ....including image files

--- app/Controller/Component/NotifyComponent.php
+++ app/Controller/Component/NotifyComponent.php
@@ -171,8 +171,8 @@ class NotifyComponent extends Component {
         $Email->template('notification','phkapa')->emailFormat('html');
         $Email->viewVars(array('notification' => $notification,'email'=>$email));
         // uncoment these lines to get more control over sender from and subject!!
-        //$Email->sender('noreply-contacta@phkapa.net', 'CONTACTA - pHKapa');
-        //$Email->from(array('noreply-contacta@phkapa.net' => 'CONTACTA - pHKapa'));
+        //$Email->sender('noreply-contacta@phkapa.net', 'pHKapa');
+        //$Email->from(array('noreply-contacta@phkapa.net' => 'pHKapa'));
         //$Email->subject(__('pHKapa').' '.__n('Notification', 'Notifications', 1));
         
         $Email->to($email);
