abdffc919e15c1e2ca274f29c4c825e2f2c048f4
pHAlkaline <contact@phalkaline.net>
2016-04-19 13:35:47 +0100

    AppModel try catch log errors on delete

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -15,6 +15,7 @@
  * @link     http://phkapa.net
  */
 App::uses('Model', 'Model');
+App::uses('CakeLog', 'Log');
 
 class AppModel extends Model {
 
@@ -38,64 +39,91 @@ class AppModel extends Model {
     public $version_request = null;
 
     /*
-    public function beforeDelete($cascade = true) {
-        if (!$this->canDelete($this->id)) {
-            return false;
-        }
-        return parent::beforeDelete($cascade);
-    }
+      public function beforeDelete($cascade = true) {
+      if (!$this->canDelete($this->id)) {
+      return false;
+      }
+      return parent::beforeDelete($cascade);
+      }
 
-    public function canDelete($id) {
+      public function canDelete($id) {
 
-        $this->name;
-        $this->id = $id;
-        $canDelete = true;
-        foreach ($this->hasMany as $model => $details) {
+      $this->name;
+      $this->id = $id;
+      $canDelete = true;
+      foreach ($this->hasMany as $model => $details) {
 
-            if ($details['dependent'] !== true && $model !== 'Comment') {
-                if ($details['className'] == $this->name) {
-                    $ModelInstance = $this;
-                } else {
-                    $ModelInstance = $this->{$model};
-                }
-                $ModelInstance->contain();
-                $count = $ModelInstance->find("count", array(
-                    "conditions" => array($details['foreignKey'] => $this->id)
-                ));
-                if ($count) {
-                    return false;
-                }
-            }
-        }
-        foreach ($this->hasAndBelongsToMany as $model => $details) {
+      if ($details['dependent'] !== true && $model !== 'Comment') {
+      if ($details['className'] == $this->name) {
+      $ModelInstance = $this;
+      } else {
+      $ModelInstance = $this->{$model};
+      }
+      $ModelInstance->contain();
+      $count = $ModelInstance->find("count", array(
+      "conditions" => array($details['foreignKey'] => $this->id)
+      ));
+      if ($count) {
+      return false;
+      }
+      }
+      }
+      foreach ($this->hasAndBelongsToMany as $model => $details) {
 
-            if (isset($details['dependent']) && $details['dependent'] == true) {
-                return $canDelete;
-            }
+      if (isset($details['dependent']) && $details['dependent'] == true) {
+      return $canDelete;
+      }
 
-            if ($details['with'] == $this->name) {
-                $ModelInstance = $this;
-            } else {
-                $ModelInstance = $this->{$details['with']};
-            }
-            $ModelInstance->contain();
-            $count = $ModelInstance->find("count", array(
-                "conditions" => array($details['foreignKey'] => $this->id)
-            ));
-            if ($count) {
-                return false;
-            }
-        }
-        return true;
-    }
-    */
-    public function delete ($id = null, $cascade = true) {
+      if ($details['with'] == $this->name) {
+      $ModelInstance = $this;
+      } else {
+      $ModelInstance = $this->{$details['with']};
+      }
+      $ModelInstance->contain();
+      $count = $ModelInstance->find("count", array(
+      "conditions" => array($details['foreignKey'] => $this->id)
+      ));
+      if ($count) {
+      return false;
+      }
+      }
+      return true;
+      }
+     */
+
+    public function delete($id = null, $cascade = true) {
         try {
-            parent::delete($id,$cascade);
-        } catch (Exception $e) {
+            parent::delete($id, $cascade);
+        } catch (Exception $exception) {
+            CakeLog::write(LOG_ERR, static::_getMessage($exception));
             return false;
         }
         return true;
+    }
+
+    /**
+     * Generates a formatted error message
+     *
+     * @param Exception $exception Exception instance
+     * @return string Formatted message
+     */
+    protected static function _getMessage($exception) {
+        $message = sprintf("[%s] %s", get_class($exception), $exception->getMessage()
+        );
+        if (method_exists($exception, 'getAttributes')) {
+            $attributes = $exception->getAttributes();
+            if ($attributes) {
+                $message .= "\nException Attributes: " . var_export($exception->getAttributes(), true);
+            }
+        }
+        if (PHP_SAPI !== 'cli') {
+            $request = Router::getRequest();
+            if ($request) {
+                $message .= "\nRequest URL: " . $request->here();
+            }
+        }
+        $message .= "\nStack Trace:\n" . $exception->getTraceAsString();
+        return $message;
     }
 
 }
