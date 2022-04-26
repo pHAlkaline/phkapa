12ecca8e22cbf31fa265c1ba3a3250587194e7f3
pHAlkaline <contact@phalkaline.net>
2022-04-27 00:40:09 +0100

    core update

--- app/Config/core.php
+++ app/Config/core.php
@@ -34,7 +34,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-Configure::write('debug', 2);
+Configure::write('debug', 0);
 Configure::write('Exception.handler', 'AppExceptionHandler::handleException');
 Configure::write('Session', array('defaults' => 'php', 'cookie' => 'pHKapaSession'));
 Configure::write('Cookie', array('name' => 'pHKapaCookie'));
